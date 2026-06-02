fd6289817a5cb31b6b297f710b45685891679429
Divyadharshini <divyad8695@gmail.com>
2026-06-02 20:21:03 +0530

    Fix API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
-const api = axios.create({ baseURL: BASE });
+const api = axios.create({ baseURL: BASE_URL });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
